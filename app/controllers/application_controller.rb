766d4d399a807939cb58b3aed07fc714ff492bae
Celia Collins <celia.collins@nesta.org.uk>
2024-07-26 12:31:20 +0100

    Put password in front of site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  if Rails.env == "production"
+    before_action :authenticate
+  end
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["USERNAME"] && password == ENV["PASSWORD"]
+    end
+  end
 end
