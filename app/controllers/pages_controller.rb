0652bdc86d264714376fe9c75d3f9f10b418e485
Celia Collins <celia.collins@nesta.org.uk>
2025-01-17 10:08:56 +0000

    Hide footer on admin pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_admin!
+  before_action :show_footer
   after_action :track_action
 
   def privacy_policy
@@ -21,5 +22,9 @@ class PagesController < ApplicationController
 
   def track_action
     ahoy.track request.path_parameters[:action], request.path_parameters
+  end
+
+  def show_footer
+    @show_footer = true
   end
 end
