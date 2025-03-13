c4d140603bba17ee1a1cb11ef7884064be0daa16
Celia Collins <celia.collins@nesta.org.uk>
2025-03-13 15:48:13 +0000

    Make factory valid

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { "Ali" }
     last_name { "Smith" }
-    sequence(:phone_number) { |n| "07#{n}23456789" }
+    sequence(:phone_number) {|n| n.to_s.rjust(11, '07') }
     child_birthday { Time.now - 18.months }
     terms_agreed_at { Time.now }
     contactable { true }
