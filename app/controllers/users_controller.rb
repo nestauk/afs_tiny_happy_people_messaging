43507c07a369e5b87f876034bc272e359584c454
Celia Collins <celia.collins@nesta.org.uk>
2025-04-15 16:28:18 +0100

    Allow texts to be sent in language of user's choosing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -119,9 +119,9 @@ class UsersController < ApplicationController
 
   def set_languages
     @languages = if params[:locale] == "cy"
-      [['Cymraeg', 'cy'], ['English', 'en']]
+      [["Cymraeg", "cy"], ["English", "en"]]
     else
-      [['English', 'en'], ['Cymraeg', 'cy']]
+      [["English", "en"], ["Cymraeg", "cy"]]
     end
   end
 end
