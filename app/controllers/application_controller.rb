db15d0d23e78f2d91f419de6c6f690bd14e4f960
Celia Collins <celia.collins@nesta.org.uk>
2025-04-15 16:28:18 +0100

    Allow texts to be sent in language of user's choosing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { locale: I18n.locale }
+    {locale: I18n.locale}
   end
 
   def after_sign_in_path_for(resource)
