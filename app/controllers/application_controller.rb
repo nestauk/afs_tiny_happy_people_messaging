351dfff0a5f1a1ed997f1951fa597ffbb7490c43
Celia Collins <celia.collins@nesta.org.uk>
2025-04-15 11:50:15 +0100

    Translate home page and add language preferences for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = extract_locale_from_params || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def after_sign_in_path_for(resource)
     if resource.role == "local_authority"
@@ -9,9 +19,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
-
   def check_admin_role
     redirect_to root_path unless current_admin.role == "admin"
+  end
+
+  private
+
+  def extract_locale_from_params
+    parsed_locale = params[:locale].to_s
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
 end
