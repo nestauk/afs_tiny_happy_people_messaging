3893f661369290fcaf0c6d54d9c37b78bb9eea59
Celia Collins <celia.collins@nesta.org.uk>
2025-04-24 11:13:25 +0100

    Move messages status update to a job
    If we're getting lots of updates from Twilio, we don't want to block
    the service with these requests.

---
+++ app/jobs/update_message_status_job.rb
@@ -0,0 +1,16 @@
+class UpdateMessageStatusJob < ApplicationJob
+  queue_as :default
+
+  def perform(params)
+    message_sid = params[:MessageSid]
+    status = params[:MessageStatus]
+
+    message = Message.find_by(message_sid:)
+
+    return if message.nil? || message.status == "delivered"
+
+    if message.update(status:)
+      message.update(sent_at: Time.zone.now) if message.status == "delivered"
+    end
+  end
+end
