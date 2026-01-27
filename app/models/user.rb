bdd15215092ed8f2bf6f4e2b6930c4318393511e
cdccollins <celia.collins@nesta.org.uk>
2026-01-27 11:43:41 +0000

    Translate rate limit exceeded

--- app/models/user.rb
+++ app/models/user.rb
@@ -123,11 +123,10 @@ class User < ApplicationRecord
   end
 
   def find_next_unseen_content
-    last_content = Content.find(last_content_id)
-    last_position = last_content.position if last_content
+    last_position = Content.find(last_content_id).position
     seen_ids = messages.where.not(content_id: nil).select(:content_id)
 
-    Group.find(last_content.group_id)
+    Group.find_by(language: language)
       .contents
       .active
       .where.not(id: seen_ids)
