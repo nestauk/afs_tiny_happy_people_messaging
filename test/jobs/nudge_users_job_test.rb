c4fca8655263204c9279b4527f6fde5b36a138fb
cdccollins <celia.collins@nesta.org.uk>
2026-03-17 16:46:52 +0000

    Change name of service

--- test/jobs/nudge_users_job_test.rb
+++ test/jobs/nudge_users_job_test.rb
@@ -7,7 +7,7 @@ class NudgeUsersJobTest < ActiveSupport::TestCase
   test "#perform updates user and sends message" do
     user = create(:user)
 
-    stub_successful_twilio_call("Welcome back to Tiny Happy People! Text 'END' to unsubscribe at any time.", user)
+    stub_successful_twilio_call("Welcome back to CBeebies Parenting! Text 'END' to unsubscribe at any time.", user)
 
     assert_enqueued_jobs 1, only: SendCustomMessageJob do
       NudgeUsersJob.new.perform(user)
