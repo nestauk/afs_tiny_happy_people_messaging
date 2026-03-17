416dafa7d1ee53c654f77db6f637db6910424520
cdccollins <celia.collins@nesta.org.uk>
2026-03-17 16:46:52 +0000

    Change name of service

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -311,7 +311,7 @@ class UserTest < ActiveSupport::TestCase
   test "#put_on_waitlist method sets user to waitlist" do
     user = create(:user, contactable: true, restart_at: nil)
 
-    stub_successful_twilio_call("Hi Ali! Thank you for signing up to the Tiny Happy People text messaging programme. We’re currently receiving a large volume of sign ups, and as a result we unfortunately will have to place you on a waiting list to receive this service. We expect that we will be able to provide the service for you starting in September provided your child is still under 24 months. Please respond STOP if you would like to opt out, otherwise we will send your first text messages in September. We hope that you will join us in the autumn!", user)
+    stub_successful_twilio_call("Hi Ali! Thank you for signing up to the CBeebies Parenting text messaging programme. We’re currently receiving a large volume of sign ups, and as a result we unfortunately will have to place you on a waiting list to receive this service. We expect that we will be able to provide the service for you starting in September provided your child is still under 24 months. Please respond STOP if you would like to opt out, otherwise we will send your first text messages in September. We hope that you will join us in the autumn!", user)
 
     user.put_on_waitlist
 
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -25,7 +25,7 @@ class UsersTest < ApplicationSystemTestCase
     check "Building a better routine with my child"
     fill_in "We're currently available in English, with more languages on the horizon! Let us know your preferred language to help shape our future offerings", with: "Polish"
 
-    stub_successful_twilio_call("Hi Jo, welcome to our programme of weekly texts with fun activities for Jack's development. Congrats on starting this amazing journey with your little one! To get started, why not save this number as 'Tiny Happy People' so you can easily see when it's us texting you?", User.new(phone_number: "+447444930200"))
+    stub_successful_twilio_call("Hi Jo, welcome to our programme of weekly texts with fun activities for Jack's development. Congrats on starting this amazing journey with your little one! To get started, why not save this number as 'CBeebies Parenting' so you can easily see when it's us texting you?", User.new(phone_number: "+447444930200"))
 
     click_button "Next"
 
@@ -72,7 +72,7 @@ class UsersTest < ApplicationSystemTestCase
 
     assert_text "You're almost done"
 
-    stub_successful_twilio_call("Hi Jo! Thank you for signing up to the Tiny Happy People text messaging programme. We’re currently receiving a large volume of sign ups, and as a result we unfortunately will have to place you on a waiting list to receive this service. We expect that we will be able to provide the service for you starting in September provided your child is still under 24 months. Please respond STOP if you would like to opt out, otherwise we will send your first text messages in September. We hope that you will join us in the autumn!", User.last)
+    stub_successful_twilio_call("Hi Jo! Thank you for signing up to the CBeebies Parenting text messaging programme. We’re currently receiving a large volume of sign ups, and as a result we unfortunately will have to place you on a waiting list to receive this service. We expect that we will be able to provide the service for you starting in September provided your child is still under 24 months. Please respond STOP if you would like to opt out, otherwise we will send your first text messages in September. We hope that you will join us in the autumn!", User.last)
 
     click_button "Skip this section"
 
@@ -93,7 +93,7 @@ class UsersTest < ApplicationSystemTestCase
 
     assert_text "You're almost done"
 
-    stub_successful_twilio_call("Hi Jo, welcome to our programme of weekly texts with fun activities for your child's development. Congrats on starting this amazing journey with your little one! To get started, why not save this number as 'Tiny Happy People' so you can easily see when it's us texting you?", User.last)
+    stub_successful_twilio_call("Hi Jo, welcome to our programme of weekly texts with fun activities for your child's development. Congrats on starting this amazing journey with your little one! To get started, why not save this number as 'CBeebies Parenting' so you can easily see when it's us texting you?", User.last)
 
     click_button "Skip this section"
 
