4fef68518547eb7f2a48416ae72803d05e94a5fb
cdccollins <celia.collins@nesta.org.uk>
2026-04-14 10:22:16 +0100

    Add link to bilingual text

--- app/jobs/send_bilingual_message_job.rb
+++ app/jobs/send_bilingual_message_job.rb
@@ -1,15 +1,18 @@
 class SendBilingualMessageJob < ApplicationJob
   include MessageVariableSubstitution
+  include Rails.application.routes.url_helpers
 
   queue_as :background
 
   def perform(user)
     message = Message.build do |m|
+      m.token = m.send(:generate_token)
       m.user = user
       m.body = substitute_variables(
         I18n.t(".messages.bilingual_text", locale: user.language || I18n.default_locale),
-        user,
+        user, token: m.token
       )
+      m.link = "https://www.bbc.co.uk/tiny-happy-people/articles/ztrj4xs"
     end
 
     if message.save
--- test/jobs/send_bilingual_message_job_test.rb
+++ test/jobs/send_bilingual_message_job_test.rb
@@ -7,12 +7,14 @@ class SendBilingualMessageJobTest < ActiveSupport::TestCase
   test "#perform sends message with default content" do
     user = create(:user, child_name: "Harry")
 
+    Message.any_instance.stubs(:generate_token).returns("ABC")
+
     assert_enqueued_jobs 1, only: SendCustomMessageJob do
       SendBilingualMessageJob.new.perform(user)
     end
 
     assert_equal 1, Message.count
-    assert_equal("Wales is a vibrant mix of many languages—and they all belong here! Speaking to Harry in your native language is a wonderful gift for their development. Learn more about bilingual households here:", Message.last.body)
+    assert_equal("Wales is a vibrant mix of many languages—and they all belong here! Speaking to Harry in your native language is a wonderful gift for their development. Learn more about bilingual households here: http://localhost:3000/m/ABC", Message.last.body)
     assert_not_nil user.sent_bilingual_text_at
   end
 
@@ -20,12 +22,14 @@ class SendBilingualMessageJobTest < ActiveSupport::TestCase
     create(:group, language: "cy")
     user = create(:user, language: "cy", child_name: "Harry")
 
+    Message.any_instance.stubs(:generate_token).returns("ABC")
+
     assert_enqueued_jobs 1, only: SendCustomMessageJob do
       SendBilingualMessageJob.new.perform(user)
     end
 
     assert_equal 1, Message.count
-    assert_equal("Mae Cymru yn gymysgedd bywiog o lawer o ieithoedd—ac maen nhw i gyd yn perthyn yma! Mae siarad â Harry yn eich iaith frodorol yn rhodd werthfawr i’w datblygiad. Dysgwch fwy am aelwydydd dwyieithog yma:", Message.last.body)
+    assert_equal("Mae Cymru yn gymysgedd bywiog o lawer o ieithoedd—ac maen nhw i gyd yn perthyn yma! Mae siarad â Harry yn eich iaith frodorol yn rhodd werthfawr i’w datblygiad. Dysgwch fwy am aelwydydd dwyieithog yma: http://localhost:3000/m/ABC", Message.last.body)
     assert_not_nil user.sent_bilingual_text_at
   end
 
