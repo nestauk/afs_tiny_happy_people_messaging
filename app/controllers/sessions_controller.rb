a946fb702046a25e19249d8fd35ce50ab030c02d
Celia Collins <celia.collins@nesta.org.uk>
2026-04-16 10:22:18 +0100

    Rate limit admin login attempts (#291)
    - Checking IP seems to not be working
    - So added rate limit to admin login attempts instead, which should work as expected

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < Devise::Passwordless::SessionsController
-  prepend_before_action :check_ip, only: [:new, :create]
+  # prepend_before_action :check_ip, only: [:new, :create]
+
+  rate_limit to: 3, within: 5.minutes, by: -> { request.ip }, only: :create, with: -> { rate_limit_exceeded }
 
   def new
     super
@@ -15,5 +17,14 @@ class SessionsController < Devise::Passwordless::SessionsController
     if Rails.env.production? && request.remote_ip != ENV.fetch("ADMIN_IP_WHITELIST")
       redirect_to root_path, alert: "Access denied."
     end
+  end
+
+  def rate_limit_exceeded
+    @user = User.new
+    @no_padding = true
+    @hide_sidebar = true
+
+    flash.now[:notice] = I18n.t("controllers.users.rate_limit_exceeded.notice")
+    redirect_to root_path, alert: I18n.t("controllers.users.rate_limit_exceeded.alert")
   end
 end
