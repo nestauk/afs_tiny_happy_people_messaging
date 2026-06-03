c1edc65e4f7a7d7e305a200c7f6a61a270787afe
cdccollins <celia.collins@nesta.org.uk>
2026-06-03 10:04:14 +0100

    Remove deprecation warning for Rails 8.2

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardsController < ApplicationController
   def show
     @hide_sidebar = true
     @q = LocalAuthority.all
-    @all_las_dashboard = AllLasDashboard.first
+    @all_las_dashboard = AllLasDashboard.take
     @specific_la_dashboards = LaSpecificDashboard.all
   end
 end
