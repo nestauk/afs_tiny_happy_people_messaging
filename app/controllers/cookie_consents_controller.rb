e3f76c7acfd6d89517250a0f6ad9538b16712b63
Celia Collins <celia.collins@nesta.org.uk>
2026-06-30 09:29:06 +0100

    Track which page user consents to double check GA numbers (#404)

--- app/controllers/cookie_consents_controller.rb
+++ app/controllers/cookie_consents_controller.rb
@@ -4,9 +4,10 @@ class CookieConsentsController < ApplicationController
   def create
     category = params[:category].to_s
     decision = params[:decision].to_s
+    page = params[:page].to_s
 
-    if %w[analytics marketing].include?(category) && %w[accepted declined].include?(decision)
-      ahoy.track "cookie_consent", category: category, decision: decision
+    if %w[analytics marketing statistical].include?(category) && %w[accepted declined].include?(decision)
+      ahoy.track "cookie_consent", page: page, category: category, decision: decision
     end
 
     head :no_content
