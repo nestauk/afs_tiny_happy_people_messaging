9f985af5d30a394820dfd9939a5d31ea2db1236d
cdccollins <celia.collins@nesta.org.uk>
2026-08-17 11:03:01 +0100

    Clean up migration files

--- db/migrate/20260805140000_rename_bulk_messages_to_broadcasts.rb
+++
@@ -1,6 +0,0 @@
-class RenameBulkMessagesToBroadcasts < ActiveRecord::Migration[8.1]
-  def change
-    rename_table :bulk_messages, :broadcasts
-    rename_column :messages, :bulk_message_id, :broadcast_id
-  end
-end
