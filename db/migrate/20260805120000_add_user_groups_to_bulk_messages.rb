883ef2c0c44e333e3255007b3c234f18142a4857
cdccollins <celia.collins@nesta.org.uk>
2026-08-17 11:03:01 +0100

    Clean up migration files

--- db/migrate/20260805120000_add_user_groups_to_bulk_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddUserGroupsToBulkMessages < ActiveRecord::Migration[8.1]
-  def change
-    add_column :bulk_messages, :user_groups, :string, array: true, default: [], null: false
-  end
-end
